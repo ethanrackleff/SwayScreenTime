8c75beb9a4d271e4b4b430e6eb75954e177c404b
ethanrackleff <ethanrackleff@gmail.com>
2025-09-13 15:48:09 -0400

    Implement initial_focus()

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+/*
+To-do:
+    - make initial_focus recursive
+*/
+
 #include <nlohmann/json.hpp> // to parse JSON files. This is a dependency
 #include <chrono> // to time how long a window has been focused
 #include <stdio.h>
@@ -6,10 +11,11 @@
 #include <fstream>
 
 using json = nlohmann::json;
+using string = std::string;
 
-std::string run_read_cmd(const std::string& cmd) {
+string run_read_cmd(const string& cmd) {
     char buffer[128]; //temporarily hold characters from command output
-    std::string output = "";
+    string output = "";
     FILE* pipe = popen(cmd.c_str(), "r"); //convert cmd to a C string and read the output into pipe
     if (!pipe)
         return "popen failed";
@@ -20,26 +26,39 @@ std::string run_read_cmd(const std::string& cmd) {
     return output;
 }
 
-//find node with "focused": true
-/*
-std::string initial_focus(const json& node) {
-    if (node.is_object()) { //Base case of recursion is node not being a JSON file
-        if (node.contains("name") && node["name"] == "root") {
-            initial_focus(node["nodes"]); //Goes into root to look for different outputs
-        }
-        if (get_tree_output.contains("focused") && get_tree_output["focused"].is_boolean() && get_tree_output["focused"].template get<bool>() == true)  {
-        return get_tree_output;
-        }  
-
+string initial_focus(const json& root) {
+    if (root["type"] == "root") {
+        std::cout << "In root\n";
+       for (auto& output : root["nodes"]) {
+           std::cout << "In output\n";
+           if (output["name"] != "__i3") {
+                for (auto& workspace : output["nodes"]) {
+                    if (workspace["type"] == "workspace") {
+                        std::cout << "In workspaces\n";
+                        for (auto& window : workspace["nodes"]) {
+                            std::cout << "In windows\n";
+                            if (window.contains("focused") && window["focused"].is_boolean() && window["focused"].template get<bool>()) {
+                                return window["app_id"];
+                            }
+                            for (auto& window2 : window["nodes"]) {
+                                if (window2.contains("focused") && window2["focused"].is_boolean() && window2["focused"].template get<bool>()) {
+                                    return window2["app_id"];
+                                }    
+                            }
+                        }
+                    }
+                }
+            }
+       }
     }
-    
-    else {
-        
-    }
+    return "Couldn't find focused window";
 }
-*/
+
 int main() {
-    std::string result = run_read_cmd("swaymsg -t get_tree");
-    std::cout << result;
+    string result = run_read_cmd("swaymsg -t get_tree");
+    std::cout << "This is the output: " << result;
+    json get_tree_output = json::parse(result);
+    string result2 = initial_focus(get_tree_output);
+    std::cout << "this is the output: " << result2 << "\n";
     return 0;
 }
